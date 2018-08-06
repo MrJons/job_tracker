aa2c55c24c4c830679b3cd1d9fb317121c970d97
Jon Shad <shad_stick@hotmail.co.uk>
2018-08-06 20:50:37 +0100

    Pass request data back into form on validation error

--- resources/views/pages/jobs.blade.php
+++ resources/views/pages/jobs.blade.php
@@ -17,37 +17,37 @@
             {{ csrf_field() }}
             <div class="form-row">
                 <div class="form-group col-md-12">
-                    <input type="text" class="form-control" name="companyName" id="companyName" placeholder="Company name">
+                    <input type="text" class="form-control" name="companyName" value="{{ old('companyName', null) }}" placeholder="Company name">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-12">
-                    <input type="url" class="form-control" name="companyURL" id="companyURL" placeholder="Company URL">
+                    <input type="url" class="form-control" name="companyURL" value="{{ old('companyURL', null) }}" placeholder="Company URL">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <input type="text" class="form-control" name="contactName" id="contactName" placeholder="Contact name">
+                    <input type="text" class="form-control" name="contactName" value="{{ old('contactName', null) }}" placeholder="Contact name">
                 </div>
                 <div class="form-group col-md-6">
-                    <input type="email" class="form-control" name="contactEmail" id="contactEmail" placeholder="Contact email">
+                    <input type="email" class="form-control" name="contactEmail" value="{{ old('contactEmail', null) }}" placeholder="Contact email">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-4">
-                    <select class="form-control" name="companyInterest" id="companyInterest">
-                        <option value="" selected disabled>Interest</option>
+                    <select class="form-control" name="companyInterest">
+                        <option selected disabled>{{ array_key_exists('companyInterest', old()) ? old('companyInterest') : 'Interest' }}</option>
                         @foreach(range(1,10) as $number)
                             <option> {{$number}} </option>
                         @endforeach
                     </select>
                 </div>
                 <div class="form-group col-md-4">
-                    <select class="form-control" name="applicationStage" id="applicationStage">
-                        <option value="" selected disabled>Application stage</option>
+                    <select class="form-control" name="applicationStage">
+                        <option selected disabled>{{ array_key_exists('applicationStage', old()) ? old('applicationStage') : 'Application stage' }}</option>
                         <option>Not applied</option>
                         <option>Applied</option>
                         <option>Tech test</option>
@@ -57,13 +57,13 @@
                     </select>
                 </div>
                 <div class="form-group col-md-4">
-                    <input type="date" class="form-control" name="lastInteraction" id="lastInteraction" placeholder="Last interaction" data-toggle="tooltip" data-placement="right">
+                    <input type="date" class="form-control" name="lastInteraction" value="{{ old('lastInteraction', null) }}" placeholder="Last interaction" data-toggle="tooltip" data-placement="right">
                 </div>
             </div>
 
             <div class="form-row">
                 <div class="form-group col-md-12">
-                    <textarea class="form-control" name="companyNotes" id="companyNotes" rows="5" placeholder="Extra Notes"></textarea>
+                    <textarea class="form-control" name="companyNotes" rows="5" placeholder="Extra Notes">{{ old('companyNotes') }}</textarea>
                 </div>
             </div>
 
