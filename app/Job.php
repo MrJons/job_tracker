e1307a44905fcfb27589b91a42ea1df3cf24f00e
mrjons <shad_stick@hotmail.co.uk>
2018-12-08 22:51:34 +0000

    Add relationships to models for User & Job

--- app/Job.php
+++ app/Job.php
@@ -4,31 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Job extends Model {
+class Job extends Model
+{
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'company_name',
-        'company_url',
-        'contact_name',
-        'contact_email',
-        'role_interest',
-        'application_stage',
-        'last_interaction',
-        'extra_notes'
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'user_id'
-    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
 }
